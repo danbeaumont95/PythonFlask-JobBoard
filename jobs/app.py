35fcc56341a83a6dbbf890c4426fdd1039dc8164
Dan Beaumont <danielbeaumont95@hotmail.co.uk>
2022-04-26 19:08:38 +0100

    added basic styling to /

--- jobs/app.py
+++ jobs/app.py
@@ -2,12 +2,8 @@ from flask import render_template, Flask
 
 app = Flask(__name__)
 
-def jobs():
-  return render_template('index.html')
 
 @app.route('/')
-
 @app.route('/jobs')
-
 def jobs():
-  return render_template('index.html')
+    return render_template('index.html')
